7604beb2aaedc7701d8057cbb2b5c05bfb5935b1
Andrius Žimkus <and.zimkus@gmail.com>
2024-12-16 20:51:19 +0200

    2024-12-16 20:51:19

--- Day16/ReindeerMaze.py
+++ Day16/ReindeerMaze.py
@@ -1,4 +1,3 @@
-
 def main():
  
     fileName = "input.txt"
@@ -25,16 +24,18 @@ def main():
     print(cp)
     print(ep)
 
+    global scoreMatrix 
     scoreMatrix = [[{} for x in range(len(maze[0]))] for x in range (len(maze))]
 
     #for row in scoreMatrix:
     #    print(row)
 
-    #First attempt - recursively
+    #First attempt - recursively - fails because of recursion limit
     #traverseMaze(maze,cp,direction,0,scoreMatrix)
 
     #Second attempt - queue
-    steps = getSteps(cp,direction,0)
+    steps = getSteps(cp,direction,0,[cp])
+    
 
     while True:
         if len(steps) == 0:
@@ -44,24 +45,45 @@ def main():
         cp = currentStep[0]
         cd = currentStep[1]
         cs = currentStep[2]
-        steps.extend(performStep(maze,cp,cd,cs,scoreMatrix))
+        stepsTaken = currentStep[3]
+        steps.extend(performStep(maze,cp,cd,cs,stepsTaken))
         
     #for row in scoreMatrix:
     #    print(row)
 
     
-    print(scoreMatrix[ep[1]][ep[0]])
+    #print(scoreMatrix[ep[1]][ep[0]])
 
     finishScores = scoreMatrix[ep[1]][ep[0]]
 
     minScore = 0
     for score in finishScores:
+        if score != "E" and score != "W" and score != "N" and score != "S":
+            continue
         if minScore == 0 or finishScores[score] < minScore:
             minScore = finishScores[score]
 
     print("Minimum score:", minScore)
 
+    print("Cell for minimum:", len(finishScores[minScore]))
 
+    #for row in maze:
+    #    print(row)
+
+    newMaze = []
+    for i in range(len(maze)):
+        currentRow = []
+        for j in range(len(maze[0])):
+            if (j,i) in finishScores[minScore]:
+                currentRow.append("O")
+            else:
+                currentRow.append(maze[i][j])
+        newMaze.append(currentRow)
+        
+
+    #for row in newMaze:
+    #    print(*row)
+    
 def traverseMaze(maze,cp,cd,cs,scoreMatrix):
 
     if maze[cp[1]][cp[0]] == "#":
@@ -70,6 +92,9 @@ def traverseMaze(maze,cp,cd,cs,scoreMatrix):
     isAdded = addScore(scoreMatrix,cp,cd,cs)
 
     if not isAdded:
+        return
+
+    if maze[cp[1]][cp[0]] == "E":
         return
 
     #Recursively go forward, go right, go left
@@ -86,35 +111,54 @@ def traverseMaze(maze,cp,cd,cs,scoreMatrix):
     newCell = getNextCell(cp, newDirection)
     traverseMaze(maze,newCell,newDirection,cs+1001,scoreMatrix)
  
-def getSteps(cp,cd,cs):
-    stepForward = (getNextCell(cp,cd),cd,cs+1)
+def getSteps(cp,cd,cs,stepsTaken):
+    
+    stepForward = (getNextCell(cp,cd),cd,cs+1,stepsTaken+[getNextCell(cp,cd)])
 
     newDirection = getNextDirection(cd,"Right")
     newCell = getNextCell(cp, newDirection)
-    stepRight = (newCell,newDirection,cs+1001)
+    stepRight = (newCell,newDirection,cs+1001,stepsTaken+[newCell])
 
     newDirection = getNextDirection(cd,"Left")
     newCell = getNextCell(cp, newDirection)
-    stepLeft = (newCell,newDirection,cs+1001)
+    stepLeft = (newCell,newDirection,cs+1001,stepsTaken+[newCell])
 
     return [stepForward,stepRight,stepLeft]
 
-def performStep(maze,cp,cd,cs,scoreMatrix):
+def performStep(maze,cp,cd,cs,stepsTaken):
+    global scoreMatrix
+    
     if maze[cp[1]][cp[0]] == "#":
         return []
     
-    isAdded = addScore(scoreMatrix,cp,cd,cs)  
+    isAdded = addScore(cp,cd,cs)  
     if not isAdded:
         return []
 
-    return getSteps(cp,cd,cs)
+    if maze[cp[1]][cp[0]] == "E":
+        #print(scoreMatrix)
+        cellScores = scoreMatrix[cp[1]][cp[0]]
+        #print("Cell scores",cellScores)
+        if cs in cellScores:
+            #print("CS already in", cs)
+            stepsAlready = cellScores[cs]
+            for step in stepsTaken:
+                if not step in stepsAlready:
+                    cellScores[cs].append(step)
+        else:
+            #print("CS",cs)
+            #print("not in")
+            #print (cellScores)
+            cellScores[cs] = stepsTaken
+        return []
+
+    return getSteps(cp,cd,cs,stepsTaken)
     
-def addScore(scoreMatrix,cp,cd,cs):
+def addScore(cp,cd,cs):
+    global scoreMatrix
     cellScores = scoreMatrix[cp[1]][cp[0]]
 
-    isOpp = isOppositeInScores(cd,cellScores)
-
-    if (not cd in cellScores or cellScores[cd] > cs):
+    if (not cd in cellScores or cellScores[cd] >= cs):
         cellScores[cd] = cs
         return True
     else:
@@ -139,12 +183,6 @@ def getNextDirection(cd,turn):
         return directions[(index+1)%len(directions)]
     elif turn == "Left":
         return directions[index-1]
-
-def isOppositeInScores(cd,cellScores):
-    return (cd == "E" and "W" in cellScores
-        or cd == "W" and "E" in cellScores
-        or cd == "N" and "S" in cellScores
-        or cd == "S" and "N" in cellScores)
 
     
 if __name__ == "__main__":
