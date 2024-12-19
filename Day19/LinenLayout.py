fed2e54a18a19a23f4956fc92c6f4061c6755d80
Andrius Žimkus <and.zimkus@gmail.com>
2024-12-19 22:22:23 +0200

    2024-12-19 22:22:23

--- Day19/LinenLayout.py
+++ Day19/LinenLayout.py
@@ -1,6 +1,6 @@
 def main():
  
-    fileName = "test.txt"
+    fileName = "input.txt"
     path = "../../Advent Of Code Cases/Day19/" + fileName
 
     designs = []
@@ -10,10 +10,69 @@ def main():
         for line in file:
             designs.append(line.strip())
 
+    global possibleDesigns
+    global designsTried
+    possibleDesigns = {}
+    designsTried = {}
     #designsPossible = countDesignsPossible_1_slow(patterns,designs)
+    designsPossible = countDesignsPossible_1_rec(patterns,designs)
+    print("Designs possible:", designsPossible)
 
-    #print("Designs possible:", designsPossible)
+    
 
+def countDesignsPossible_1_rec(patterns,designs):
+    global designsTried
+    i = 1
+    designsPossible = 0
+    for design in designs:
+        #print(i)
+        #print(design)
+        designsTried = {}
+        isCurrentPossible = isDesignPossible_rec(design,patterns,[])
+        #print(isCurrentPossible)
+        if isCurrentPossible:
+            designsPossible+=1
+        i+=1
+        
+    return designsPossible
+
+def isDesignPossible_rec(design,patterns,designSequence):
+    global possibleDesigns
+    global designsTried
+    #print(designSequence)
+    #print(designsTried)
+    
+    #print("D",design)
+    designsToTry = []
+    designsTried[design] = "True"
+    for pattern in patterns:
+        if design == pattern:
+            for d in designSequence:
+                possibleDesigns[d] = True
+            return True
+        elif design in possibleDesigns:
+            return True
+        elif design[:len(pattern)] == pattern:
+            leftoverDesign = design[len(pattern):]
+            if not leftoverDesign in designsTried:
+                #print("adding",leftoverDesign)
+                designsToTry.append((leftoverDesign,designSequence + [leftoverDesign]))
+    #print(designsToTry)
+
+    if len(designsToTry) == 0:
+        return False
+    else:
+        for dtt in designsToTry:
+            dtt_o = dtt[0]
+            ds = dtt[1]
+            inp = isDesignPossible_rec(dtt_o,patterns,ds)
+            if inp:
+                for d in ds:
+                    possibleDesigns[d] = True
+                return True
+
+    return False
+    
 def countDesignsPossible_1_slow(patterns,designs):
 
     patternsGrouped = {}
