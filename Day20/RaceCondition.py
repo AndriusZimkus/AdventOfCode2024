68bf03e4fddf84d3978ef883a8fdeda67e0058ac
Andrius Žimkus <and.zimkus@gmail.com>
2024-12-21 15:07:43 +0200

    2024-12-21 15:07:43

--- Day20/RaceCondition.py
+++ Day20/RaceCondition.py
@@ -1,6 +1,6 @@
 def main():
  
-    fileName = "test.txt"
+    fileName = "test2.txt"
     path = "../../Advent Of Code Cases/Day20/" + fileName
 
     raceTrack = []
@@ -16,34 +16,274 @@ def main():
             if raceTrack[i][j] == "S":
                 sp = (j,i)
 
+    #Part 1
     global cheatsPossible
     global cheatsAdded
+    global initDistances
     cheatsPossible = []
     cheatsAdded = {}
-    #initDistances = [[0 for x in range(len(raceTrack[0]))] for x in range(len(raceTrack))]    
-    #for row in raceTrack:
-    #    print(row)
-    initDistances = traverseRaceTrack(raceTrack,sp)
-    #print(ep)
-    #print(sp)
-
     
+    initDistances = traverseRaceTrack(raceTrack,sp)
     initialDistance = initDistances[ep[1]][ep[0]]
     print("Initial distance:",initialDistance)
-    #for row in initDistances:
-    #    print(row)
+    
+    saves = {}
+    savesAtLeast100 = 0
+    for cheat in cheatsPossible:
+        startPos = cheat[0]
+        cheatPos = cheat[1]
+        direction = cheat[2]
+        x = cheatPos[0]
+        y = cheatPos[1]
 
-    #Determine cheats and traverse
-    print(cheatsPossible)
+        cheatFinishPos = getNextCellPos(raceTrack,x,y,direction)
+
+        startPosValue = getMatrixCell(initDistances,startPos)
+        finishPosValue = getMatrixCell(initDistances,cheatFinishPos)
+
+        saveValue = finishPosValue - startPosValue - 2
         
+        if saveValue >= 100:
+            savesAtLeast100 += 1
+        saves[saveValue] = saves.get(saveValue, 0) + 1
+        
+
+    print("Cheats possible:",len(cheatsPossible))
+    print("Saves at least 100:",savesAtLeast100)
+
+    #Part 2
+    #Determine all possible cheats
+    saves = {}
+    cheats2 = getCheats_Part2(raceTrack,sp)
+    #print("Printing cheats")
+    for cheat in cheats2:
+        print(cheat)
+        finishPositionsConsidered = []
+        startPos = cheat[0]
+        startPosDist = getMatrixCell(initDistances,startPos)
+
+        finishPositions = cheat[1]
+        for fp in finishPositions:
+            fpPos = fp[0]
+            fpStepCount = fp[1]
+            if fpPos in finishPositionsConsidered:
+                continue
+            finishPositionsConsidered.append(fpPos)
+            finishPosDist = getMatrixCell(initDistances,fpPos)
+
+            saveValue = finishPosDist-startPosDist - fpStepCount
+
+            #if saveValue >= 50:
+            saves[saveValue] = saves.get(saveValue, 0) + 1
+                
+    print(saves)
+
+
+def getCheats_Part2(raceTrack,sp):
+    steps = getSteps(sp,0)
+    cheats = []
+
+    nextCell = getMatrixCell(raceTrack,sp)
+    currentPos = sp
+
+    #Traverse track and determine all cheats for all cells
+    while nextCell != "E":
+
+
+        cheatsForCell = getCheatsForCell(raceTrack,currentPos)
+
+        if len(cheatsForCell) > 0:
+            cheats.append((currentPos,cheatsForCell))
+        
+        nextTrackPos = getNextTrackPos(raceTrack,currentPos)
+
+        nextCell = getMatrixCell(raceTrack,nextTrackPos)
+        currentPos = nextTrackPos
+        
+        #print(nextCell)
+
+    return cheats
+
+def getCheatsForCell(raceTrack,pos):
+    #BFS - with queue - second attempt
+
+    return []    
+
+    #Recursively determine possible cheats - first attempt
+##    global visitedCellsDists
+##    visitedCellsDists = {}
+##    finPosOfCheats = []
+##    initialDistance = getMatrixCell(initDistances,pos)
+##    initials = getInitials(raceTrack,pos)
+##    for initial in initials:
+##
+##        finPosOfCheats.extend(getCheatsForCell_rec(raceTrack,initial,1,initialDistance,pos))
+##
+##    if len(finPosOfCheats)>0:
+##        return finPosOfCheats
+##    else:
+##        return []
+
+def getCheatsForCell_rec(raceTrack,pos,step,startDist,fromPos):
+    global visitedCellsDists
+    x = pos[0]
+    y = pos[1]
+    c = len(raceTrack[0])
+    r = len(raceTrack)
+
+    if x == 0 or y == 0 or x == c-1 or y == r-1:
+        return []
+
+    if step >= 20:
+        return []
+
+    if pos in visitedCellsDists and visitedCellsDists[pos] <= step:
+        return []
+
+    visitedCellsDists[pos] = step
+
+    finPosOfCheats = []
+
+    nextPositions = [(x+1,y),(x-1,y),(x,y+1),(x,y-1)]
+    nextPositions.remove(fromPos)
+
+    #Check next positions - end of cheat or continue cheat
+    for nextPos in nextPositions:
+        nextCell = getMatrixCell(raceTrack,nextPos)
+        
+        if nextCell == "." or nextCell == "E":
+            #End of cheat
+            nextCellDist = getMatrixCell(initDistances,nextPos)
+
+            if (nextCellDist - startDist - (step+1)) > 0:
+
+                finPosOfCheats.append((nextPos,step+1))
+        elif nextCell == "#":
+            #Cheat continues:
+            finPosOfCheats.extend(getCheatsForCell_rec(raceTrack,nextPos,step+1,startDist,pos))
+            
+    return finPosOfCheats
+
+    
+
+def getInitials(raceTrack,pos):
+    x = pos[0]
+    y = pos[1]
+    initials = []
+
+    #Up
+    try:
+        nextPosX = x
+        nextPosY = y-1
+        nextCell = raceTrack[nextPosY][nextPosX]
+        if nextCell == "#":
+            initials.append((nextPosX,nextPosY))
+    except:
+        pass
+
+    #Down
+    try:
+        nextPosX = x
+        nextPosY = y+1
+        nextCell = raceTrack[nextPosY][nextPosX]
+        if nextCell == "#":
+            initials.append((nextPosX,nextPosY))
+    except:
+        pass
+
+    #Left
+    try:
+        nextPosX = x-1
+        nextPosY = y
+        nextCell = raceTrack[nextPosY][nextPosX]
+        if nextCell == "#":
+            initials.append((nextPosX,nextPosY))
+    except:
+        pass
+
+    #Right
+    try:
+        nextPosX = x+1
+        nextPosY = y
+        nextCell = raceTrack[nextPosY][nextPosX]
+        if nextCell == "#":
+            initials.append((nextPosX,nextPosY))
+    except:
+        pass
+
+    return initials
+
+def getNextTrackPos(raceTrack,pos):
+    global initDistances
+    x = pos[0]
+    y = pos[1]
+    posDist = initDistances[y][x]
+    
+    #Up
+    try:
+        nextPosX = x
+        nextPosY = y-1
+        nextCell = raceTrack[nextPosY][nextPosX]
+        nextPosDist = initDistances[nextPosY][nextPosX]
+        if (nextCell == "." or nextCell == "E") and nextPosDist > posDist:
+            return (nextPosX,nextPosY)
+    except:
+        pass
+
+    #Down
+    try:
+        nextPosX = x
+        nextPosY = y+1
+        nextCell = raceTrack[nextPosY][nextPosX]
+        nextPosDist = initDistances[nextPosY][nextPosX]
+        if (nextCell == "." or nextCell == "E") and nextPosDist > posDist:
+            return (nextPosX,nextPosY)
+    except:
+        pass
+
+    #Left
+    try:
+        nextPosX = x-1
+        nextPosY = y
+        nextCell = raceTrack[nextPosY][nextPosX]
+        nextPosDist = initDistances[nextPosY][nextPosX]
+        if (nextCell == "." or nextCell == "E") and nextPosDist > posDist:
+            return (nextPosX,nextPosY)
+    except:
+        pass
+
+    #Right
+    try:
+        nextPosX = x+1
+        nextPosY = y
+        nextCell = raceTrack[nextPosY][nextPosX]
+        nextPosDist = initDistances[nextPosY][nextPosX]
+        if (nextCell == "." or nextCell == "E") and nextPosDist > posDist:
+            return (nextPosX,nextPosY)
+    except:
+        pass
+
+    
+    
+def getMatrixCell(matrix,pos):
+    x = pos[0]
+    y = pos[1]
+    cell = ""
+    try:
+        cell = matrix[y][x]
+    except:
+        pass
+    
+    return cell
 
 
 def traverseRaceTrack(raceTrack,sp):
     distances = [
-        [0 for x in range(len(raceTrack[0]))]
+        [-1 for x in range(len(raceTrack[0]))]
         for x in range(len(raceTrack))]
 
     steps = getSteps(sp,0)
+    distances[sp[1]][sp[0]] = 0
     
     def performStep(step):
         nonlocal distances
@@ -73,23 +313,21 @@ def traverseRaceTrack(raceTrack,sp):
             nY = nextCellPos[1]
             try:
                 nextCell = raceTrack[nY][nX]
-                print("NC for cheat", nextCell)
-                print("nY", nY)
-                print("nX", nX)
-                if nextCell == "." and nY >= 0 and nx >= 0:
-                    print("CA",cheatsAdded)
-                    if not nextCellPos in cheatsAdded:
-                        cheatsAdded[nextCellPos] = True
-                        cheatsPossible.append((startPos,pos))
+
+                if (nextCell == "." or nextCell == "E") and nY >= 0 and nX >= 0:
+ 
+                    if not pos in cheatsAdded:
+                        cheatsAdded[pos] = True
+                        cheatsPossible.append((startPos,pos,direction))
             except:
                 pass
                 
             return []
 
-        if distances[y][x] != 0 and currDist >= distances[y][x]:
+        if distances[y][x] != -1 and currDist >= distances[y][x]:
             return []
 
-        if distances[y][x] == 0 or currDist < distances[y][x]:
+        if distances[y][x] == -1 or currDist < distances[y][x]:
             distances[y][x] = currDist
 
         if cell == "E":
