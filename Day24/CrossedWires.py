3c945630c1ecf0249aedb005c838e5aa5dad1b68
Andrius Žimkus <and.zimkus@gmail.com>
2025-01-05 18:12:06 +0200

    2025-01-05 18:12:06

--- Day24/CrossedWires.py
+++ Day24/CrossedWires.py
@@ -9,9 +9,10 @@ def main():
     wires = {}
     connections = []
     relations = {}
-
+    xConns = []
     with open(path, 'r') as file:
-        currentNumber = 0
+        cID = 0
+        
         for line in file:
             line = line.strip().split(":")
 
@@ -19,6 +20,7 @@ def main():
             if len(line) == 2:
                 wires[line[0]] = int(line[1])
             elif len(line) == 1 and line[0] != "":
+                
                 line = line[0]
                 line = line.split("->")
                 condition = line[0].split(" ")
@@ -28,18 +30,30 @@ def main():
                 second = condition[2]
                 logic = condition[1]
 
+                if second < first:
+                    first,second = second,first
+
+
+
+                currConn = Connection(first,second,result,logic,cID)
+                cID+=1
+                connections.append(currConn)
+
                 if first in relations:
-                    relations[first].append(result)
+                    relations[first].append(currConn)
                 else:
-                    relations[first] = [result]
+                    relations[first] = [currConn]
 
                 if second in relations:
-                    relations[second].append(result)
+                    relations[second].append(currConn)
                 else:
-                    relations[second] = [result]
+                    relations[second] = [currConn]
 
-                connections.append(Connection(first,second,result,logic))
+                if first[0] == "x":
+                    xConns.append(currConn)
+                    
 
+    
     newWires = updateWires(wires,connections)
 
     zWGN = int(getWireGroupNumber(newWires,"z"),2)
@@ -47,31 +61,83 @@ def main():
     yWGN = int(getWireGroupNumber(newWires,"y"),2)
     
     print("Part 1:", zWGN)
-    #print("X", xWGN)
-    #print("Y", yWGN)
-    #print("Z", zWGN)
-    #print("Part 2:",xWGN + yWGN == zWGN)
-    
-    print("Connection count", len(connections))
+    print("Unchanged wires, x",xWGN)
+    print("Unchanged wires, y",yWGN)
+    print("Unchanged wires, z",zWGN)
+    print("Unchanged wires, result:", xWGN + yWGN == zWGN)
 
+    #Finding erros in wiring by hand and looking, i.e. manually
+    i1 = 7
+    i2 = 154
     
-    i1 = 0
-    i2 = 1
-    #print(connections[i1])
-    #print(connections[i2])
-    connections[i1].result,connections[i2].result = connections[i2].result,connections[i1].result
-    #print(connections[i1])
-    #print(connections[i2])
-    newWires2 = updateWires(wires,connections)
+    connections[i1].result,connections[i2].result \
+    = connections[i2].result, connections[i1].result
+
+    i3 = 33
+    i4 = 75
+
+    connections[i3].result,connections[i4].result \
+    = connections[i4].result, connections[i3].result
+
+    i5 = 68
+    i6 = 79
+
+    connections[i5].result,connections[i6].result \
+    = connections[i6].result, connections[i5].result
+
+    i7 = 114
+    i8 = 136
+
+    connections[i7].result,connections[i8].result \
+    = connections[i8].result, connections[i7].result
+
+    newWires = updateWires(wires,connections)
+
+    zWGN = int(getWireGroupNumber(newWires,"z"),2)
+    xWGN = int(getWireGroupNumber(newWires,"x"),2)
+    yWGN = int(getWireGroupNumber(newWires,"y"),2)
     
-    zWGN = int(getWireGroupNumber(newWires2,"z"),2)
-    xWGN = int(getWireGroupNumber(newWires2,"x"),2)
-    yWGN = int(getWireGroupNumber(newWires2,"y"),2)
-    
-    print("X", xWGN)
-    print("Y", yWGN)
-    print("Z", zWGN)
+
+    print("Changed wires, x",xWGN)
+    print("Changed wires, y",yWGN)
+    print("Changed wires, z",zWGN)
+    print("Changed wires, result:", xWGN + yWGN == zWGN)
     print("Part 2:",xWGN + yWGN == zWGN)
+
+    part2CXNS = [
+        connections[i1].result,
+        connections[i2].result,
+        connections[i3].result,
+        connections[i4].result,
+        connections[i5].result,
+        connections[i6].result,
+        connections[i7].result,
+        connections[i8].result,
+        ]
+
+    part2CXNS.sort()
+    print(",".join(part2CXNS))
+    
+    #print(connections[i1].result)
+    #print(connections[i2].result)
+    #print(connections[i3].result)
+    #print(connections[i4].result)
+    
+##    print("Connection count", len(connections))
+##    xConns.sort()
+##    for cxn in xConns:
+##        print("------")
+##        print(cxn)
+##        
+##        currResult = cxn.result
+##        try:
+##            currRelations = relations[currResult]
+##            print("Related")
+##            for cR in currRelations:
+##                print(cR)
+##        except:
+##            pass
+
 
 
 def updateWires(wires,connections):
@@ -121,15 +187,16 @@ def getWireGroupNumber(wires,group):
     return binaryNumber
             
 class Connection:
-    def __init__(self,first,second,result,logic):
+    def __init__(self,first,second,result,logic,cID):
         self.first = first
         self.second = second
         self.result = result
         self.logic = logic
+        self.cID = cID
 
     def __str__(self):
         return (self.first + " " + self.logic + " " + self.second
-                + " -> " + self.result)
+                + " -> " + self.result + " " + str(self.cID))
 
     def evaluate(self,wires):
 
@@ -154,6 +221,13 @@ class Connection:
                 return 0
         else:
             assert False
+
+    def __lt__(self, other):
+         return (self.first < other.first
+                 or (self.first == other.first and
+                     self.logic > other.logic)
+                     
+                 )
         
         
         
